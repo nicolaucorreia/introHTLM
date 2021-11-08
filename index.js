3d63ded424bc3e00be7858ecfb56eff051bc78cb
nicolaucorreia <nicolau.correia95@gmail.com>
2021-11-08 12:39:47 -0100

    criando navbar e personalizando cachorro

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { constants } = require('buffer')
 const express = require('express')
 const path = require('path')
 const app = express()
@@ -8,5 +9,8 @@ app.use(express.static('./public'))
   response.sendFile(path.join(_dirname, '/public/index.html'))
 }) 
  
+const port = 3000;
 
-app.listen(3000)
+app.listen(port, function (){
+console.log("Lisenning on port: " + port)
+})
